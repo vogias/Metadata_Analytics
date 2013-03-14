b987cc7efed90c74a964f026eb00312cd024daf5
vogias <vogias@admin.grnet.gr>
2013-03-14 10:36:37 +0200

    c

---
+++ Metadata_Analytics/src/analytics/storage/store2csv.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package analytics.storage;
+
+import java.util.HashMap;
+
+/**
+ * @author vogias
+ * 
+ */
+public class store2csv extends Storage {
+
+	String providerName;
+
+	public store2csv(String providerName) {
+		// TODO Auto-generated constructor stub
+		this.providerName = providerName;
+	}
+
+	@Override
+	public void storeData(HashMap<String, Double> data) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
