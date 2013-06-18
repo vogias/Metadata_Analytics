3af51eb6b7d60e4c18bdd28e1773a077aa3189eb
vogias <vogias@admin.grnet.gr>
2013-06-18 15:09:04 +0300

    stupid problem fixed

--- Metadata_Analytics/src/analytics/constants/AnalyticsConstants.java
+++ Metadata_Analytics/src/analytics/constants/AnalyticsConstants.java
@@ -11,7 +11,7 @@ public class AnalyticsConstants {
 
 	public static String inputClass = "analytics.input.data";
 	public static String storageClass = "analytics.storage";
-	public static String mdstorePath = "analytics.analysis.path";
+	public static String mdstorePath = "analytics.mdstore.path";
 	public static String mdHandler = "analytics.mdstore.data.handler";
 	public static String elementValues = "analytics.element.values";
 	
